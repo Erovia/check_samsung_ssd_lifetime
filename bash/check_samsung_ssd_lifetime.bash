91ca1372beed05bd537531c5808c7451140235c8
Erovia <erovia@users.noreply.github.com>
2025-03-03 12:03:15 +0000

    Add support for NVMe drives

--- bash/check_samsung_ssd_lifetime.bash
+++ bash/check_samsung_ssd_lifetime.bash
@@ -19,7 +19,12 @@ if [[ "$#" -ne "1" ]]; then
 	exit 1
 fi
 DEVICE="$1"
-if [[ ! -b "$DEVICE" ]]; then
+if [[ "$DEVICE" == "/dev/nvme"* ]]; then
+	if [[ ! -b "${DEVICE}n1" ]]; then
+		echo -e "${RED}ERROR:${RESET} '$DEVICE' does not seem to be a drive!"
+		exit 2
+	fi
+elif [[ ! -b "$DEVICE" ]]; then
 	echo -e "${RED}ERROR:${RESET} '$DEVICE' does not seem to be a drive!"
 	exit 2
 fi
@@ -54,12 +59,10 @@ get_smart_value() {
 SMART_INFO=$($SUDO $SMARTCTL -x "$DEVICE")
 
 while read line; do
+	# SATA SSDs
 	if [[ "$line" == "Device Model"* ]]; then
 		DEVICE_MODEL="${line/"Device Model:"/}"
 		DEVICE_MODEL="$(trim_whitespaces "$DEVICE_MODEL")"
-	elif [[ "$line" == "Serial Number"* ]]; then
-		SERIAL_NUMBER="${line/"Serial Number:"/}"
-		SERIAL_NUMBER="$(trim_whitespaces "$SERIAL_NUMBER")"
 	elif [[ "$line" == "Sector Size"* ]]; then
 		LBA_SIZE="${line/"Sector Size:"/}"
 		LBA_SIZE="${LBA_SIZE/"bytes logical/physical"/}"
@@ -70,6 +73,35 @@ while read line; do
 		WEAR_LEVELING_COUNT="$(get_smart_value "$line" 3)"
 	elif [[ "$line" == *"Total_LBAs_Written"* ]]; then
 		TOTAL_LBAS_WRITTEN="$(get_smart_value "$line" 7)"
+
+	# NVMe SSDs
+	elif [[ "$line" == "Model Number"* ]]; then
+		DEVICE_MODEL="${line/"Model Number:"/}"
+		DEVICE_MODEL="$(trim_whitespaces "$DEVICE_MODEL")"
+	elif [[ "$line" == "Namespace 1 Formatted LBA Size"* ]]; then
+		LBA_SIZE="${line/"Namespace 1 Formatted LBA Size:"/}"
+		LBA_SIZE="$(trim_whitespaces "$LBA_SIZE")"
+	elif [[ "$line" == "Power On Hours"* ]]; then
+		POWER_ON_HOURS="${line/"Power On Hours:"/}"
+		POWER_ON_HOURS="$(trim_whitespaces "$POWER_ON_HOURS")"
+	elif [[ "$line" == "Percentage Used"* ]]; then
+		WEAR_LEVELING_COUNT="${line/"Percentage Used:"/}"
+		WEAR_LEVELING_COUNT="${WEAR_LEVELING_COUNT%\%}"    # Remove % sign from the end
+		WEAR_LEVELING_COUNT="$(trim_whitespaces "$WEAR_LEVELING_COUNT")"
+		WEAR_LEVELING_COUNT=$(( 100 - WEAR_LEVELING_COUNT ))    # NVMe drives provide the used percentage
+	elif [[ "$line" == "Data Units Written"* ]]; then
+		TOTAL_LBAS_WRITTEN="${line/"Data Units Written:"/}"
+		TOTAL_LBAS_WRITTEN="${TOTAL_LBAS_WRITTEN%[*}"    # Remove the trailing TB conversion
+		TOTAL_LBAS_WRITTEN="${TOTAL_LBAS_WRITTEN//,}"    # Remove the , delimiters
+		TOTAL_LBAS_WRITTEN="$(trim_whitespaces "$TOTAL_LBAS_WRITTEN")"
+	elif [[ "$line" == "Available Spare:"* ]]; then    # The ':' is necessary to avoid matching other spare-related entries
+		AVAILABLE_SPARE="${line/"Available Spare:"/}"
+		AVAILABLE_SPARE="$(trim_whitespaces "$AVAILABLE_SPARE")"
+
+	# Common
+	elif [[ "$line" == "Serial Number"* ]]; then
+		SERIAL_NUMBER="${line/"Serial Number:"/}"
+		SERIAL_NUMBER="$(trim_whitespaces "$SERIAL_NUMBER")"
 	fi
 done <<< $SMART_INFO
 
@@ -82,11 +114,14 @@ fi
 # Calculations
 ##########################################
 
-TOTAL_BYTES_WRITTEN=$(( $TOTAL_LBAS_WRITTEN * $LBA_SIZE ))
-TOTAL_MB_WRITTEN=$(( $TOTAL_BYTES_WRITTEN / 1024 ))
-TOTAL_GB_WRITTEN=$(( $TOTAL_MB_WRITTEN / 1024 ))
-TOTAL_TB_WRITTEN=$(( $TOTAL_GB_WRITTEN / 1024 ))
-MEAN_WRITE_RATE=$(( $TOTAL_MB_WRITTEN / $POWER_ON_HOURS ))
+TOTAL_BYTES_WRITTEN=$(( TOTAL_LBAS_WRITTEN * LBA_SIZE ))
+TOTAL_MB_WRITTEN=$(( TOTAL_BYTES_WRITTEN / 1000 ))
+TOTAL_MIB_WRITTEN=$(( TOTAL_BYTES_WRITTEN / 1024 ))
+TOTAL_GB_WRITTEN=$(( TOTAL_MB_WRITTEN / 1000 ))
+TOTAL_GIB_WRITTEN=$(( TOTAL_MIB_WRITTEN / 1024 ))
+TOTAL_TB_WRITTEN=$(( TOTAL_GB_WRITTEN / 1000 ))
+TOTAL_TIB_WRITTEN=$(( TOTAL_GIB_WRITTEN / 1024 ))
+MEAN_WRITE_RATE=$(( TOTAL_MB_WRITTEN / POWER_ON_HOURS ))
 
 # Convert the value to Base10 in order to trim the leading 0
 HEALTH_PERCENTAGE=$(( 10#$WEAR_LEVELING_COUNT ))
@@ -107,9 +142,10 @@ echo "Model: $DEVICE_MODEL"
 echo "Serial number: $SERIAL_NUMBER"
 printf "Power on time: %'d hours\n" "$POWER_ON_HOURS"
 echo "Data written:"
-printf "    MB: %'d\n" "$TOTAL_MB_WRITTEN"
-printf "    GB: %'d\n" "$TOTAL_GB_WRITTEN"
-printf "    TB: %'d\n" "$TOTAL_TB_WRITTEN"
+printf "    MB: %'d [%'d MiB]\n" "$TOTAL_MB_WRITTEN" "$TOTAL_MIB_WRITTEN"
+printf "    GB: %'d [%'d GiB]\n" "$TOTAL_GB_WRITTEN" "$TOTAL_GIB_WRITTEN"
+printf "    TB: %'d [%'d TiB]\n" "$TOTAL_TB_WRITTEN" "$TOTAL_TIB_WRITTEN"
 echo "Mean write rate:"
 printf "    MB/hr: %'d\n" "$MEAN_WRITE_RATE"
+[[ -n "$AVAILABLE_SPARE" ]] && echo "Available spare capacity: $AVAILABLE_SPARE"
 echo -e "Estimated drive health: ${HEALTH_COLOUR}${HEALTH_PERCENTAGE}${RESET}%"
--- python/check_samsung_ssd_lifetime.py
+++ python/check_samsung_ssd_lifetime.py
@@ -36,6 +36,8 @@ def sanity_checks(device):
         exit(3)
 
 def check_if_drive(device):
+    if 'nvme' in device:
+        device += 'n1'
     return Path(device).is_block_device()
 
 def check_for_sudo():
@@ -58,10 +60,9 @@ def get_smart_attrs(device):
         exit(3)
 
     for line in exe.stdout.split('\n'):
+        # SATA SSDs
         if line.startswith('Device Model'):
             attrs['device_model'] = ' '.join(line.split()[2:])
-        elif line.startswith('Serial Number'):
-            attrs['serial_number'] = line.split()[2]
         elif line.startswith('Sector Size'):
             attrs['lba_size'] = int(line.split()[2])
         elif 'Power_On_Hours' in line:
@@ -71,7 +72,25 @@ def get_smart_attrs(device):
         elif 'Total_LBAs_Written' in line:
             attrs['total_lbas_written'] = int(line.split()[7])
 
-    if 'samsung' not in attrs['device_model'].lower():
+        # NVMe SSDs
+        elif line.startswith('Model Number'):
+            attrs['device_model'] = ' '.join(line.split()[2:])
+        elif line.startswith('Namespace 1 Formatted LBA Size'):
+            attrs['lba_size'] = int(line.split()[-1])
+        elif line.startswith('Power On Hours'):
+            attrs['power_on_hours'] = int(line.split()[3])
+        elif line.startswith('Percentage Used'):
+            attrs['wear_leveling_count'] = 100 - int(line.split()[2][:-1])
+        elif line.startswith('Data Units Written'):
+            attrs['total_lbas_written'] = int(line.split()[3].replace(',', ''))
+        elif line.startswith('Available Spare:'):
+            attrs['available_spare'] = line.split()[2]
+
+        # Common
+        elif line.startswith('Serial Number'):
+            attrs['serial_number'] = line.split()[2]
+
+    if 'samsung' not in attrs.get('device_model', '').lower():
         print(f'{RED}ERROR:{RESET} This script is only compatible with Samsung SSDs!')
         exit(4)
 
@@ -85,9 +104,12 @@ def get_smart_attrs(device):
 def calculate_usage(smart_attrs):
     usage = {}
     usage['total_bytes_written'] = smart_attrs['total_lbas_written'] * smart_attrs['lba_size']
-    usage['total_mb_written'] = usage['total_bytes_written'] / 1024
-    usage['total_gb_written'] = usage['total_mb_written'] / 1024
-    usage['total_tb_written'] = usage['total_gb_written'] / 1024
+    usage['total_mb_written'] = usage['total_bytes_written'] / 1000
+    usage['total_mib_written'] = usage['total_bytes_written'] / 1024
+    usage['total_gb_written'] = usage['total_mb_written'] / 1000
+    usage['total_gib_written'] = usage['total_mib_written'] / 1024
+    usage['total_tb_written'] = usage['total_gb_written'] / 1000
+    usage['total_tib_written'] = usage['total_gib_written'] / 1024
     usage['mean_write_rate'] = usage['total_mb_written'] / smart_attrs['power_on_hours']
     return usage
 
@@ -105,17 +127,22 @@ def health_colour(health):
         return GREEN
 
 def print_output(device, smart_attrs, usage):
+    if available_spare := smart_attrs.get('available_spare', False):
+        available_spare = f'Available spare capacity: {available_spare}\n'
+    else:
+        available_spare = ''
+
     print(f'''Device: {device}
 Model: {smart_attrs['device_model']}
 Serial number: {smart_attrs['serial_number']}
 Power on time: {smart_attrs['power_on_hours']:,} hours
 Data written:
-    MB: {int(usage['total_mb_written']):,}
-    GB: {int(usage['total_gb_written']):,}
-    TB: {int(usage['total_tb_written']):,}
+    MB: {int(usage['total_mb_written']):,} [{int(usage['total_mib_written']):,} MiB]
+    GB: {int(usage['total_gb_written']):,} [{int(usage['total_gib_written']):,} GiB]
+    TB: {int(usage['total_tb_written']):,} [{int(usage['total_tib_written']):,} TiB]
 Mean write rate:
     MB/hr: {int(usage['mean_write_rate']):,}
-Estimated drive health: {health_colour(smart_attrs['wear_leveling_count'])}{smart_attrs['wear_leveling_count']}{RESET}%''')
+{available_spare}Estimated drive health: {health_colour(smart_attrs['wear_leveling_count'])}{smart_attrs['wear_leveling_count']}{RESET}%''')
 
 
 def main():
